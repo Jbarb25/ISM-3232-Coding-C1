013bdf5721357064379e18aac7fbe11d04973d9a
Jbarb25 <barboza205@usf.edu>
2025-01-21 16:37:19 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+let employeeName= "Mickey Mouse";
+const employeeID= 2345;
+var isActive= true;
+console.log(typeof(employeeName))
+console.log(employeeName)
+console.log(typeof(employeeID))
+console.log(employeeID)
+console.log(typeof(isActive))
+console.log(isActive)
+
+
